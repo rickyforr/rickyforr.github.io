9480f3960b5593f93f25cae9541b1e4b476fec92
rickyforr <richard.forrester@gmail.com>
2020-10-23 16:48:53 -0600

    update to resume

---
+++ precache-manifest.2dfff7422789e113c6a3b62373220076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df58bc66d5273255eb8a553e991ba728",
+    "url": "./index.html"
+  },
+  {
+    "revision": "674233491c50f083421a",
+    "url": "./static/css/main.e8fa0ed4.chunk.css"
+  },
+  {
+    "revision": "cd2b8f4cf33ffbcab0d5",
+    "url": "./static/js/2.d2b4523a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d2b4523a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "674233491c50f083421a",
+    "url": "./static/js/main.701379e1.chunk.js"
+  },
+  {
+    "revision": "4442f186521b7bcc4b4f",
+    "url": "./static/js/runtime-main.ca8ab1f2.js"
+  },
+  {
+    "revision": "8386b49bee459af14a07e18f1bf3dd86",
+    "url": "./static/media/walkSprite.8386b49b.png"
+  }
+]);

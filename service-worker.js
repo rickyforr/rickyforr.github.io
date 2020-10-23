7ef4570c5a0bf2eb2ad019900c6026647c869a78
rickyforr <richard.forrester@gmail.com>
2020-10-23 16:51:50 -0600

    update resume game

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2dfff7422789e113c6a3b62373220076.js"
+  "./precache-manifest.6f925693ede7f0b6efc9a4304c2dd998.js"
 );
 
 self.addEventListener('message', (event) => {

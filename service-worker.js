1cfd7782eed60ca469830783e99efd640f67c47d
rickyforr <richard.forrester@gmail.com>
2020-10-29 10:25:04 -0600

    added title

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6d39974313ac50c32952b676d5153243.js"
+  "./precache-manifest.d1fea5339e76cdc58ed201b343b2db7d.js"
 );
 
 self.addEventListener('message', (event) => {
